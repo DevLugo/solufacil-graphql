72afc9231a05cbc4df16dbb12fbae5570f28d178
DevLugo <bl1ndcod3@gmail.com>
2022-05-23 23:37:06 -0500

    chore: debug

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { parse } from 'dotenv';
 import path from 'path';
 
 async function bootstrap() {
+    const v8 = require('v8');
+    // Output available memory in GBs
+    console.log(v8.getHeapStatistics().heap_size_limit / 1024 ** 3);
     const app = await NestFactory.create(AppModule);
     await app.listen(process.env.PORT || 4000);
 }
